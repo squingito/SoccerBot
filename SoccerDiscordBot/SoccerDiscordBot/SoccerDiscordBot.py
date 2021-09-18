a03e7f969cd86912faba9262527d3919093657f2
squingito <squingito@gmail.com>
2021-09-18 14:21:36 -0400

    added new file database

--- SoccerDiscordBot/SoccerDiscordBot/SoccerDiscordBot.py
+++ SoccerDiscordBot/SoccerDiscordBot/SoccerDiscordBot.py
@@ -1,23 +1,21 @@
 import discord
 import asyncio
-
-
+import dataBase
 
 client = discord.Client()
 
-async def InputHandler():
-    def check(message):
-        return message.author != client.user
-    try:
-        message = await client.wait_for('message', check=check,timeout = 300)
-    except asyncio.TimeoutError:
-        return None, 'noResponce'
-    else:
-        print(message.author.nick)
-        print(message)
-        if "" in message.content:
-            return message, ""
+async def send(text):
+    await message.channel.send(text)
 
+async def inputHandler(message):
+    if "%getStats" in message.content and (len(message.content) >= 9):
+        print("this")
+        await send(dataBase.getStats(message.content))
+    elif '%compare' in message.content and (len(message.content) >= 10):
+        await send(dataBase.comapreMethod(message))
+    else:
+        await message.channel.send("Request format is %Command then args sepreated by spaces")
+    
 @client.event
 async def on_ready():
     print("Bot Running")
@@ -25,6 +23,6 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author != client.user:
-        await message.channel.send(message.content);
+        await inputHandler(message)
 
 client.run("ODg4ODA2MDU0ODE1NzMxNzMz.YUYDJA.-peMP7SORqlIhQVj2D4QcDkXvBU")
