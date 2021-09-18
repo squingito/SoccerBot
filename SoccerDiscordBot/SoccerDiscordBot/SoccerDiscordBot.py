5311f775d11da009039315ecd8aa88f6e39caaf8
squingito <squingito@gmail.com>
2021-09-18 16:39:08 -0400

    added diffrent format methods

--- SoccerDiscordBot/SoccerDiscordBot/SoccerDiscordBot.py
+++ SoccerDiscordBot/SoccerDiscordBot/SoccerDiscordBot.py
@@ -4,15 +4,15 @@ import dataBase
 
 client = discord.Client()
 
-async def send(text):
+async def send(message,text):
     await message.channel.send(text)
 
 async def inputHandler(message):
     if "%getStats" in message.content and (len(message.content) >= 9):
         print("this")
-        await send(dataBase.getStats(message.content))
-    elif '%compare' in message.content and (len(message.content) >= 10):
-        await send(dataBase.comapreMethod(message))
+        await send(message,dataBase.getStats(message.content))
+    elif '%compare' in message.content and (len(message.content) >= 10 and "|" in message.content):
+        await send(message,dataBase.compare(message.content))
     else:
         await message.channel.send("Request format is %Command then args sepreated by spaces")
     
--- SoccerDiscordBot/SoccerDiscordBot/dataBase.py
+++ SoccerDiscordBot/SoccerDiscordBot/dataBase.py
@@ -1,10 +1,10 @@
 from openpyxl import load_workbook
+
 workbook = load_workbook(filename='PL_dataset.xlsx')
 workbook.sheetnames
 
 sheet = workbook.active
 
-print(sheet["B4"].value)
 
 #sheet["A"]
 def findIndexOf(name):
@@ -14,7 +14,7 @@ def findIndexOf(name):
                                  min_col = 1, max_col = 1,
                                  values_only = True):
         tempValue = value[0]
-        if (userInput == tempValue):
+        if (name.lower() == tempValue.lower()):
             rowVar1 = i
             valueFound = True
             break
@@ -25,21 +25,48 @@ def findIndexOf(name):
         return None
 
 def getStats(name):
-    print("here")
-    name = name.split(" ")[1]
-    num = findIndexOf(name)
-    if(num != None):
-        return formatGetStats(num)
+    if(len(name.split(" ")) >= 2):
+        name = name[10:]
+        print(name)
+        num = findIndexOf(name)
+        if(num != None):
+            text = formatGetStats(num) + "\n"
+            if sheet["D" + str(num)].value == "Goalkeeper":
+                text = text + "\n" + sheet["D" + str(num)].value + " Stats:\n"
+                text = text + goalkeeperFormatGetStats(num)
+            elif sheet["D" + str(num)].value == "Defender":
+                text = text + "\n" + sheet["D" + str(num)].value + " Stats:\n"
+                text = text + defenderFormatGetStats(num)
+            elif sheet["D" + str(num)].value == "Forward":
+                print('here')
+                text = text + "\n" + sheet["D" + str(num)].value + " Stats:\n"
+                text = text + forwardFormatGetStats(num)
+            elif sheet["D" + str(num)].value == "Midfielder":
+                text = text + "\n" + sheet["D" + str(num)].value + " Stats:\n"
+                text = text + midfeildFormatGetStats(num)
+            return text
+        else:
+            return '"' + name + '"' + " does not exist"
     else:
-        return '"' + name + '"' + " does not exist"
+        return "please write a person after the space"
 
 def compare(names):
-    name1 = names.split(" ")[1]
-    name2 = names.split(" ")[2]
+    name1 = names.split("|")[0][9:]
+    name2 = names.split("|")[1]
+    while(1):
+        if name1[-1] == " ":
+            name1 = name1[:-1]
+        else:
+            break
+    while(1):
+        if name2[0] == " ":
+            name2 = name2[1:]
+        else:
+            break
     num1 = findIndexOf(name1)
     num2 = findIndexOf(name2)
     if num1 != None and num2 != None:
-        return formatCompare(num1,num2)
+        return formatGetStats(num1) + "\n" + "\n" + formatGetStats(num2)
     elif num1 == None and num2 == None:
         return '"' + name1 + '"'+ ' and ' +  '"' + name2 + '"'  + " do not exist"
     elif num1 == None:
@@ -50,4 +77,22 @@ def compare(names):
 
 
 def formatGetStats(index):
-    return "Name: {}/nGoals: {}/nAsists: {}".format(sheet[" "+ index],sheet[" "+ index],sheet[" "+ index])  
+    return "Name: {}\nGoals: {}\nAssists: {}\nYellow Cards: {}\nRed Cards: {}\nPasses: {}\nFouls: {}\nOffsides: {}\nAppearances: {}\nAge: {}"\
+        .format(sheet["A"+ str(index)].value,sheet["J"+ str(index)].value, \
+        sheet["AC"+ str(index)].value,sheet["AL"+ str(index)].value, sheet["AM"+ str(index)].value, sheet["AD"+ str(index)].value, \
+        sheet["AN" + str(index)].value, sheet["AO" + str(index)].value, sheet["G" + str(index)].value, sheet["F" + str(index)].value) 
+
+def forwardFormatGetStats(index):
+    return "Shots on Target: {}\nShooting Accuracy (%): {}\nAerial Battles Won: {}\nAerial Battles Lost: {}\nBig Chances Created: {}\nCrosses: {}".format(sheet["L"+ str(index)].value,sheet["M"+ str(index)].value,sheet["Y"+ str(index)].value,sheet["Z"+ str(index)].value,sheet["AE"+ str(index)].value,sheet["AF"+ str(index)].value)
+    
+def midfeildFormatGetStats(index):
+    return "Tackles: {}\nTackle Success (%): {}\nThrough Balls: {}\nInterceptions: {}\nRecoveries: {}\nBig Chances Created: {}\nCrosses: {}\nAccurate Long Balls: {}".format(sheet["P" + str(index)].value,sheet["Q" + str(index)].value,sheet["AH" + str(index)].value,sheet["S" + str(index)].value,sheet["U" + str(index)].value,sheet["AE" + str(index)].value,sheet["AF" + str(index)].value,sheet["AI" + str(index)].value)
+
+def defenderFormatGetStats(index):
+    return "Clean Sheets {}\nGoals Conceded {}\nTackles {}\nTackle Success % {}\nBlocked Shots {}\nInterceptions {}\nClearances {}\nOwn Goals {}\nErrors Leading to Goal {}"\
+    .format(sheet["N" + str(index)], sheet["O" + str(index)], sheet["P" + str(index)], sheet["Q" + str(index)],\
+    sheet["R" + str(index)], sheet["S" + str(index)], sheet["T" + str(index)], sheet["AA" + str(index)], sheet["AB" + str(index)])
+
+def goalkeeperFormatGetStats(index):
+    return "Clean Sheets {}\nGoals Conceded {}\nSaves {}\nPenalties Saved {}"\
+    .format(sheet["N" + str(index)], sheet["O" + str(index)], sheet["AJ" + str(index)], sheet["AK" + str(index)])
