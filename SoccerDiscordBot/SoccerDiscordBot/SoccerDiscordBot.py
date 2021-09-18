0dddd74b934e64b45edc443a819aa279620de8a7
squingito <squingito@gmail.com>
2021-09-18 19:03:36 -0400

    updated functions

--- SoccerDiscordBot/SoccerDiscordBot/SoccerDiscordBot.py
+++ SoccerDiscordBot/SoccerDiscordBot/SoccerDiscordBot.py
@@ -9,11 +9,37 @@ async def send(message,text):
 
 async def inputHandler(message):
     if "%getStats" in message.content and (len(message.content) >= 9):
-        print("this")
-        await send(message,dataBase.getStats(message.content))
+        a1,a2,name = dataBase.getStats(message.content)
+        if name ==None:
+            await message.channel.send(a1)
+        else:
+            embedVar = discord.Embed(title=name, colour=discord.Colour(0xFF015B))
+            embedVar.add_field(name="Name:", value=a1, inline = True)
+            embedVar.add_field(name=name, value=a2, inline = True)
+            embedVar.set_footer(text= client.user.name, icon_url = client.user.avatar_url)
+            await message.channel.send(embed=embedVar)
+
     elif '%compare' in message.content and (len(message.content) >= 10 and "|" in message.content):
-        await send(message,dataBase.compare(message.content))
-    else:
+        a1,a2,a3,pos,name1,name2 = dataBase.compare(message.content)
+        if a2 == None:
+            await send(message,a1)
+        elif(pos != None):
+            embedVar = discord.Embed(title=pos + " Comparison", colour=discord.Colour(0xFF015B))
+            embedVar.add_field(name="Names:", value=a1, inline = True)
+            embedVar.add_field(name=name1, value=a2, inline = True)
+            embedVar.add_field(name=name2, value=a3, inline = True)
+            embedVar.set_footer(text= client.user.name, icon_url = client.user.avatar_url)
+            await message.channel.send(embed=embedVar)
+        else:
+            embedVar = discord.Embed(title = "Comparison", colour=discord.Colour(0xFF015B))
+            embedVar.add_field(name="Names:", value=a1, inline = True)
+            embedVar.add_field(name=name1, value=a2, inline = True)
+            embedVar.add_field(name=name2, value=a3, inline = True)
+            embedVar.set_footer(text= client.user.name, icon_url = client.user.avatar_url)
+            await message.channel.send(embed=embedVar)
+    elif "hello" in message.content:
+        await send(message,"```diff\n -" + "hello" + "\n```" + "```css\n" + "[world]" + "\n```")
+    elif "%getStats" in message.content or "%comparison" in message.content:
         await message.channel.send("Request format is %Command then args sepreated by spaces")
     
 @client.event
--- SoccerDiscordBot/SoccerDiscordBot/dataBase.py
+++ SoccerDiscordBot/SoccerDiscordBot/dataBase.py
@@ -4,6 +4,11 @@ workbook = load_workbook(filename='PL_dataset.xlsx')
 workbook.sheetnames
 
 sheet = workbook.active
+getStatsArray = ["F", "E", "G", "J", "AC", "AL", "AM", "AD", "AN"]
+defender =  ["N", "O", "P", "Q", "R", "S", "T", "AA", "AB"]
+midfielder = ["P", "Q", "AH", "S" "U", "AE", "AF", "AI", "AO"]
+goalkeeper = ["N", "O", "AJ", "AK"]
+forward = ["K", "L", "M", "S", "AE", "AF", "AO"]
 
 
 #sheet["A"]
@@ -28,27 +33,43 @@ def getStats(name):
     if(len(name.split(" ")) >= 2):
         name = name[10:]
         print(name)
+        if(name == "clown"):
+            print(name)
+            name = "Granit Xhaka"
+        if(name == "goat"):
+            name = "Harry Kane"
+
+        statArray = None
+        a1 = ""
+        a2 = ""
         num = findIndexOf(name)
+        formatType = sheet["D" + str(num)].value
         if(num != None):
-            text = formatGetStats(num) + "\n"
-            if sheet["D" + str(num)].value == "Goalkeeper":
-                text = text + "\n" + sheet["D" + str(num)].value + " Stats:\n"
-                text = text + goalkeeperFormatGetStats(num)
-            elif sheet["D" + str(num)].value == "Defender":
-                text = text + "\n" + sheet["D" + str(num)].value + " Stats:\n"
-                text = text + defenderFormatGetStats(num)
-            elif sheet["D" + str(num)].value == "Forward":
-                print('here')
-                text = text + "\n" + sheet["D" + str(num)].value + " Stats:\n"
-                text = text + forwardFormatGetStats(num)
-            elif sheet["D" + str(num)].value == "Midfielder":
-                text = text + "\n" + sheet["D" + str(num)].value + " Stats:\n"
-                text = text + midfeildFormatGetStats(num)
-            return text
+            for each in getStatsArray:
+                a1 = a1 + (str(sheet[each+str(1)].value) + ":\n")
+                a2 = a2 + (str(sheet[each+str(num)].value) +"\n")
+            if formatType == "Goalkeeper":
+                statArray = goalkeeper
+                pos = "Goalkeeper"
+            elif formatType == "Defender":
+                statArray = defender
+                pos = "Defender"
+            elif formatType == "Forward":
+                statArray = forward
+                pos = "Forward"
+            elif formatType == "Midfielder":
+                statArray = midfielder
+                pos = "Midfieler"
+
+            if(statArray != None):
+                for each in statArray:
+                    a1 = a1 + (str(sheet[each+str(1)].value) + ":\n")
+                    a2 = a2 + (str(sheet[each+str(num)].value) +"\n")
+                return a1,a2, (str(sheet["A" + str(num)].value))
         else:
-            return '"' + name + '"' + " does not exist"
+            return '"' + name + '"' + " does not exist",None,None
     else:
-        return "please write a person after the space"
+        return "please write a person after the space",None,None
 
 def compare(names):
     name1 = names.split("|")[0][9:]
@@ -63,16 +84,54 @@ def compare(names):
             name2 = name2[1:]
         else:
             break
+    text = ""
+    formatType = None
     num1 = findIndexOf(name1)
     num2 = findIndexOf(name2)
+    statArray = None
+    a1 = ""
+    a2 = ""
+    a3 = ""
+    pos = None
+    
     if num1 != None and num2 != None:
+        if(sheet["D" + str(num1)].value == sheet["D" + str(num2)].value):
+            formatType = sheet["D" + str(num1)].value
+        for each in getStatsArray:
+            a1 = a1 + (str(sheet[each+str(1)].value) + ":\n")
+            a2 = a2 + (str(sheet[each+str(num1)].value) +"\n")
+            a3 = a3 + (str(sheet[each+str(num2)].value) + "\n")
+        if formatType == "Goalkeeper":
+            statArray = goalkeeper
+            pos = "Goalkeeper"
+        elif formatType == "Defender":
+            statArray = defender
+            pos = "Defender"
+        elif formatType == "Forward":
+            statArray = forward
+            pos = "Forward"
+        elif formatType == "Midfielder":
+            statArray = midfielder
+            pos = "Midfieler"
+
+        if(statArray != None):
+            for each in statArray:
+                a1 = a1 + (str(sheet[each+str(1)].value) + ":\n")
+                a2 = a2 + (str(sheet[each+str(num1)].value) +"\n")
+                a3 = a3 + (str(sheet[each+str(num2)].value) + "\n")
+
+        if(pos != None):
+            return a1,a2,a3,pos,name1,name2
+        else:
+            return a1,a2,a3, None,name1,name2
+
         return formatGetStats(num1) + "\n" + "\n" + formatGetStats(num2)
     elif num1 == None and num2 == None:
-        return '"' + name1 + '"'+ ' and ' +  '"' + name2 + '"'  + " do not exist"
+        return '"' + name1 + '"'+ ' and ' +  '"' + name2 + '"'  + " do not exist",None,None,None,None,None
     elif num1 == None:
-        return '"' + name1 + '"' + " does not exist"
+        return '"' + name1 + '"' + " does not exist",None,None,None,None,None
     else:
-        return '"' + name2 + '"' + " does not exist"
+        return '"' + name2 + '"' + " does not exist",None,None,None,None,None
         
 
 
@@ -85,7 +144,7 @@ def formatGetStats(index):
 def forwardFormatGetStats(index):
     return "Shots on Target: {}\nShooting Accuracy (%): {}\nAerial Battles Won: {}\nAerial Battles Lost: {}\nBig Chances Created: {}\nCrosses: {}".format(sheet["L"+ str(index)].value,sheet["M"+ str(index)].value,sheet["Y"+ str(index)].value,sheet["Z"+ str(index)].value,sheet["AE"+ str(index)].value,sheet["AF"+ str(index)].value)
     
-def midfeildFormatGetStats(index):
+def midfieldFormatGetStats(index):
     return "Tackles: {}\nTackle Success (%): {}\nThrough Balls: {}\nInterceptions: {}\nRecoveries: {}\nBig Chances Created: {}\nCrosses: {}\nAccurate Long Balls: {}".format(sheet["P" + str(index)].value,sheet["Q" + str(index)].value,sheet["AH" + str(index)].value,sheet["S" + str(index)].value,sheet["U" + str(index)].value,sheet["AE" + str(index)].value,sheet["AF" + str(index)].value,sheet["AI" + str(index)].value)
 
 def defenderFormatGetStats(index):
