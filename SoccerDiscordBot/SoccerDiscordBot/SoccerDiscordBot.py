2941f28047ccb65249302fd0f1802d6efbb8a4c1
squingito <squingito@gmail.com>
2021-09-18 11:40:14 -0400

    init commit

---
+++ SoccerDiscordBot/SoccerDiscordBot/SoccerDiscordBot.py
@@ -0,0 +1,30 @@
+import discord
+import asyncio
+
+
+
+client = discord.Client()
+
+async def InputHandler():
+    def check(message):
+        return message.author != client.user
+    try:
+        message = await client.wait_for('message', check=check,timeout = 300)
+    except asyncio.TimeoutError:
+        return None, 'noResponce'
+    else:
+        print(message.author.nick)
+        print(message)
+        if "" in message.content:
+            return message, ""
+
+@client.event
+async def on_ready():
+    print("Bot Running")
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        await message.channel.send(message.content);
+
+client.run("ODg4ODA2MDU0ODE1NzMxNzMz.YUYDJA.2wu-xD-Hn2mBs_Vbv3QyCX6p4cU")
